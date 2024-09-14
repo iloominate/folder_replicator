bbaa97443d9abd413e07432cf331f315758e007f
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-09-14 18:21:28 +0200

    arguments parsing added

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--Source", help="Provide path to source folder")
+parser.add_argument("-r", "--Replica", help="Provide path to replica folder")
+parser.add_argument("-i", "--Interval", help="Provide synchronization interval")
+parser.add_argument("-l", "--Log", help="Provide path to a log file")
+
+
+def replicate():
+    args_parsed = vars(parser.parse_args())
+    print(f"Source:{args_parsed['Source']}")
+    print(f"Replica:{args_parsed['Replica']}")
+    print(f"Interval:{args_parsed['Interval']}")
+    print(f"Log file:{args_parsed['Log']}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    replicate()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
