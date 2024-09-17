829a3c8499f03d78381bbd94fbdb7da88d0e0ea7
xklima34 <xklima34@stud.fit.vutbr.cz>
2024-09-17 16:40:45 +0200

    compare_folder_content_recursive function added

--- main.py
+++ main.py
@@ -78,6 +78,47 @@ def compare_file_content(s_file_path, r_file_path):
 
 def compare_folder_content_recursive(source_path, replica_path):
 
+    # I need to make a dictionary that will track file names in the current folder:
+    source_items = []
+    with (os.scandir(source_path) as s_entries):
+        with os.scandir(replica_path) as r_entries:
+            for s_entry in s_entries:   # Go through each source folder item
+                if s_entry.is_dir():    # Item is a folder
+                    for r_entry in r_entries:   # Look for the folder name in replica
+                        if r_entry.name == s_entry.name:
+                            source_items.append(s_entry.name)
+                            if r_entry.is_dir():    # Folder with the same name was found
+                                compare_folder_content_recursive(s_entry.path, r_entry.path)
+                            else:   # File with the same name was found
+                                delete_file(r_entry.path)
+                            break
+                    # Folder with the same name was not found in replica folder
+                    copy_folder_recursive(s_entry)
+                    source_items.append(s_entry.name)
+                else:   # Item is a file
+                    file_found = False
+                    for r_entry in r_entries:
+                        if r_entry.name == s_entry.name:
+                            file_found = True
+                            source_items.append(s_entry.name)
+                            if r_entry.is_file():
+                                compare_file_content(s_entry.path, r_entry.path)
+                            else:
+                                delete_folder_recursive(r_entry.path)
+                            break
+                    if not file_found:  # File with the same name was not found in replica folder
+                        copy_file(s_entry.path, s_entry.name)
+                        source_items.append(s_entry.name)
+
+        # Delete files and folders that do not exist in source:
+        with os.scandir(replica_path) as r_entries_updated:
+            for r_entry in r_entries_updated:
+                if r_entry.name not in source_items:
+                    if r_entry.is_file():
+                        delete_file(r_entry.path)
+                    else:
+                        delete_folder_recursive(r_entry.path)
+
 
 def replicate(source, replica):
     if not os.path.isdir(source):
